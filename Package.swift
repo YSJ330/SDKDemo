831118cf6ea00538296469e18a34a6e2c5c50dd1
yu-sj <yu-sj@neusoft.com>
2024-11-29 14:12:11 +0800

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,19 @@
+import PackageDescription
+
+let package = Package(
+    name: "SDKDemo",
+    platforms: [
+        .iOS(.v16)
+    ],
+    products: [
+        .library(
+            name: "SDKDemo",
+            targets: ["SDKDemo"]),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "SDKDemo",
+            url: "./SDKDemo.xcframework"
+        )
+    ]
+)
