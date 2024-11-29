68a62f3d2177597e1f6a60ef50154186b7d853c2
yu-sj <yu-sj@neusoft.com>
2024-11-29 14:34:03 +0800

    Modify Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/tonymillion/Reachability", .branch("master")),
+        .package(url: "https://github.com/tonymillion/Reachability", branch: "master"),
     ],
     targets: [
         .binaryTarget(
