56097137a0ed3f46038d051219779eb4c7c6b027
yu-sj <yu-sj@neusoft.com>
2024-12-02 11:13:23 +0800

    Modify Package.swift

--- Package.swift
+++ Package.swift
@@ -2,24 +2,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "SDKDemoSPM",
+    name: "SDKDemo",
     platforms: [
         .iOS(.v16)
     ],
     products: [
-        .library(name: "SDKDemoSPM", targets: ["SDKDemoSPM"]),
         .library(name: "SDKDemo", targets: ["SDKDemo"])
     ],
-    dependencies: [
-        .package(url: "https://github.com/tonymillion/Reachability", branch: "master"),
-    ],
     targets: [
-        .target(
-            name: "SDKDemoSPM",
-            dependencies: [
-                .product(name: "Reachability", package: "Reachability")
-            ]
-        ),
         .binaryTarget(
             name: "SDKDemo",
             path: "./SDKDemo.xcframework"
